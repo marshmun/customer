a0ebc593d826325d83e545ae723fdb520ff4bea2
Marshall Munoz <marshmun@gmail.com>
2017-08-03 14:08:25 -0500

    repositorys added.

---
+++ src/main/java/com/example/customer/model/Customer.java
@@ -0,0 +1,49 @@
+package com.example.customer.model;
+
+public class Customer {
+    int id;
+    String firstname;
+    String lastname;
+    String phone;
+    String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
